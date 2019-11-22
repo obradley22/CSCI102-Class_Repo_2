681b0951131002b167832c36d97a8a21be6190f2
Paris Porter Bradley <obradley22@gmail.com>
2019-11-21 21:23:19 -0700

    Added third and fourth functions

--- Week12-utility.py
+++ Week12-utility.py
@@ -4,3 +4,24 @@
 
 def PrintOutput(string):
     return print("OUTPUT " + str(string))
+
+def LoadFile(file):
+    with open(file) as file_text:
+        lines = file_text.readlines()
+    return linesgit 
+
+def UpdateString(my_string, letter, index):
+    my_list = []
+    for i in my_string:
+        my_list.append(i)
+    my_list[index]=letter
+    new_string="".join(my_list)
+    return print("OUTPUT",new_string)
+
+
+def FindWordCount(list1,string):
+    count=0
+    for i in list1:
+        if string in i:
+            count+=1
+    return count
